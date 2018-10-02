c8dedeb93258877766e908f7b940681c9b61ddcc
Andrew T <andrew.tran.438@my.csun.edu>
2018-10-02 10:41:06 -0700

    test

--- app/src/androidTest/java/userdatabase/tables/userTables.java
+++ app/src/androidTest/java/userdatabase/tables/userTables.java
@@ -2,8 +2,4 @@ package userdatabase.tables;
 
 public class userTables {
 
-    public static final String CMD_CREATE_TABLE = "CREATE TABLE IF NOT EXIST"
-            +
-
-//            tesyseysy
 }
